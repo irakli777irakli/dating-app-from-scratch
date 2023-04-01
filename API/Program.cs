51355c7c05cfecaed76b27b1b562142e2ba15f5f
ikako <ikakocore@gmail.com>
2023-04-01 16:22:07 +0400

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,13 +9,15 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
-
+// get post put allowed
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
