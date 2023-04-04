5bb17d30c875ba17a64c407689810e0d0f27bcee
ikako <ikakocore@gmail.com>
2023-04-04 17:46:49 +0400

    entity relationship

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices(builder.Configuration);
@@ -14,7 +16,6 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-
 app.UseMiddleware<ExceptionMiddleware>();
 
 // get post put allowed
@@ -24,5 +25,18 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try{
+    // apply migrations and seed data
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}catch(Exception ex){
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured durning migration");
+}
+
 
 app.Run();
